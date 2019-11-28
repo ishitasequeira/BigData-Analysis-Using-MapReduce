b3efb8490b6fdabfd7a338050ce1d55d6f086c10
Ishita Sequeira <sequeira.i@northeastern.edu>
2019-11-28 11:09:09 -0500

    added logic for count analysis in hadoop

--- FinalProject/src/main/java/CountMapReduce/CountMapper.java
+++ FinalProject/src/main/java/CountMapReduce/CountMapper.java
@@ -1,4 +1,19 @@
 package CountMapReduce;
 
-public class CountMapper {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class CountMapper extends Mapper<LongWritable,Text, Text, IntWritable> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+       String str  = String.valueOf(value);
+       String [] tokens = str.split(",");
+       //count according to topic
+        context.write(new Text(tokens[5]),new IntWritable(1));
+    }
 }
--- FinalProject/src/main/java/CountMapReduce/CountReducer.java
+++ FinalProject/src/main/java/CountMapReduce/CountReducer.java
@@ -1,4 +1,21 @@
 package CountMapReduce;
 
-public class CountReducer {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class CountReducer extends Reducer<Text, IntWritable,Text, IntWritable> {
+    IntWritable result = new IntWritable();
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int count = 0;
+        for(IntWritable v : values){
+            count += v.get();
+        }
+        result.set(count);
+        context.write(key,result);
+    }
 }
