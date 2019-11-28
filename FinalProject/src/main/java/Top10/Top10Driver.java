5806724932b1149418538d03f30f2ac792777296
Ishita Sequeira <sequeira.i@northeastern.edu>
2019-11-28 17:47:51 -0500

    Added top 10 and removed unwanted files

--- FinalProject/src/main/java/Top10/Top10Driver.java
+++ FinalProject/src/main/java/Top10/Top10Driver.java
@@ -1,4 +1,63 @@
 package Top10;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.chain.ChainMapper;
+import org.apache.hadoop.mapreduce.lib.chain.ChainReducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
 public class Top10Driver {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration configuration = new Configuration();
+
+        Job job = Job.getInstance(configuration);
+        job.setJobName("part6-Counting Ip Addresses");
+        job.setJarByClass(Top10.Top10Driver.class);
+
+        Configuration mapConf1 = new Configuration(false);
+        ChainMapper.addMapper(job, Top10Mapper1.class, LongWritable.class, Text.class,
+                Text.class, Text.class,  mapConf1);
+
+        Configuration reduceConf1 = new Configuration(false);
+        ChainReducer.setReducer(job, Top10Reducer.class, Text.class, IntWritable.class,
+                Text.class, Text.class, reduceConf1);
+
+        Configuration mapConf2 = new Configuration(false);
+        ChainReducer.addMapper(job, TopMapper2.class, Text.class, Text.class,
+                NullWritable.class, Text.class,  mapConf2);
+
+//        Configuration reduceConf2 = new Configuration(false);
+//        ChainReducer.setReducer(job, MyReducer2.class, NullWritable.class, Text.class,
+//                NullWritable.class, Text.class, reduceConf2);
+
+        Path inputPath = new Path(args[0]);
+        Path outputPath = new Path(args[1]);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(Text.class);
+
+        FileInputFormat.addInputPath(job, inputPath);
+        FileOutputFormat.setOutputPath(job, outputPath);
+
+        FileSystem hdfs = FileSystem.get(configuration);
+        if(hdfs.exists(outputPath)) {
+            hdfs.delete(outputPath,true);
+        }
+
+        int code = job.waitForCompletion(true)?0:1;
+
+        System.exit(code);
+
+    }
 }
--- FinalProject/src/main/java/Top10/Top10Reducer.java
+++ FinalProject/src/main/java/Top10/Top10Reducer.java
@@ -1,4 +1,23 @@
 package Top10;
 
-public class Top10Reducer {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class Top10Reducer  extends Reducer<Text, Text, Text, Text> {
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        int sum = 0;
+        int count = 0;
+        for(Text i : values) {
+            String [] tokens = i.toString().split(",");
+            sum += Integer.parseInt(tokens[0]);
+            count+= Integer.parseInt(tokens[1]);
+        }
+//        System.out.println(new String(String.valueOf(key)));
+        float avg = (sum*1.0f)/count;
+        System.out.println(key + " --> " + avg);
+        context.write(key, new Text(String.valueOf(avg)));
+    }
 }
