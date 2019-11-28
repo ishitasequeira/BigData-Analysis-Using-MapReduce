3559d9ff8b527d4aec1cffb6c46555efd98426b3
Ishita Sequeira <sequeira.i@northeastern.edu>
2019-11-27 20:08:48 -0500

    added the dataset script

---
+++ dataset.sh
@@ -0,0 +1,8 @@
+FILES="./dataset.csv"
+for f in $FILES;
+do
+  echo "Pocessing $f file ...";
+  #set MONGODB_HOME environment variable  to point to the MongoDB installatio folder.
+  ls -l $f;
+  mongoimport --db projectdb --collection dataset --type csv --headerline $f;
+done
